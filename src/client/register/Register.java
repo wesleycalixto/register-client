8608162d774d693862c3db14b907c808ee90c5cd
wesleycalixto <weslleyezequiel32@gmail.com>
2024-09-06 17:51:29 -0300

    bug fixes

--- src/client/register/Register.java
+++ src/client/register/Register.java
@@ -1,6 +1,6 @@
-
 package client.register;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Register {
     public static void main(String[] args) throws IOException {
         Scanner register = new Scanner(System.in);
+
         /**
          * Pegando a quantidade de clientes a serem registrados
          */
@@ -15,33 +16,37 @@ public class Register {
         int numClients = register.nextInt();
         register.nextLine();
 
-        FileWriter writer = new FileWriter("C:\\Users\\Wesley\\Documents\\estudos java\\teste\\clients.csv", true);
-        writer.write("name,age,phone,gender,\n");
+        // Caminho do arquivo CSV
+        String filePath = "C:\\Users\\Wesley\\Documents\\estudos java\\teste\\clients.csv";
+        File file = new File(filePath);
+
+
+        FileWriter writer = new FileWriter(filePath, true);
+
+        // Escreve o cabeçalho apenas se o arquivo estiver vazio
+        if (file.length() == 0) {
+            writer.write("name,age,phone,gender,\n");
+        }
 
         /*
-         *Pegando dados de usuarios:
+         * Pegando dados de usuários:
          */
         for (int i = 0; i < numClients; i++) {
-            System.out.println("Informe os dados do cliente" + ( i + 1) + ":" );
-            System.out.println("Nome: ");
+            System.out.println("Informe os dados do cliente " + (i + 1) + ":");
+            System.out.print("Nome: ");
             String name = register.next();
-            System.out.println("Idade: ");
+            System.out.print("Idade: ");
             int age = register.nextInt();
-            System.out.println("Telefone? ");
+            System.out.print("Telefone: ");
             String phone = register.next();
-            System.out.println("Sexo? ");
+            System.out.print("Sexo: ");
             String gender = register.next();
-
 
             String data = name + "," + age + "," + phone + "," + gender + "\n";
             writer.write(data);
         }
-        writer.close(); //Fecha o arquivo
+
+        writer.close(); // Fecha o arquivo
         System.out.println("Dados adicionados! ");
-
-
     }
-
-    ;
-
 }
